613fe074cb59b1b5d7d3058db7c1aeab28049be6
Damien <damien_16@hotmail.ch>
2017-08-30 22:19:23 +0100

    Added project portfolio page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project page',
+    projectMessage: 'Here is a portfolio of my projects'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Something went wrong'
